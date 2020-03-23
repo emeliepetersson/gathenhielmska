8dabfb070f9687bdaca21ee3c9b4460dbc20c309
Emelie Petersson <54805510+emeliepetersson@users.noreply.github.com>
2020-03-23 14:30:17 +0100

    Create booking.php

---
+++ public/themes/Gathenhielmska/page-templates/booking.php
@@ -0,0 +1,20 @@
+<?php /* Template name: Booking */ ?>
+
+<?php get_header(); ?>
+<?php if (have_posts()) : ?>
+
+    <div>
+
+        <?php while (have_posts()) : the_post(); ?>
+
+            <div>
+                <h1><?php the_title(); ?></h1>
+                <?php the_content(); ?>
+            </div>
+
+        <?php endwhile; ?>
+
+    </div>
+<?php endif; ?>
+
+<?php get_footer(); ?>
