8ce9141f1ac7da10aa00bb53ab91acfc48f1531a
emeliepetersson <emepet1204@skola.goteborg.se>
2020-03-25 17:48:19 +0100

    update plus-icon, add icon to search form and update booking.php

--- public/themes/Gathenhielmska/page-templates/booking.php
+++ public/themes/Gathenhielmska/page-templates/booking.php
@@ -8,7 +8,6 @@
         <?php while (have_posts()) : the_post(); ?>
 
             <div>
-                <h1><?php the_title(); ?></h1>
                 <?php the_content(); ?>
             </div>
 
--- public/themes/Gathenhielmska/searchform.php
+++ public/themes/Gathenhielmska/searchform.php
@@ -1,4 +1,4 @@
 <form class="search-form" action="/" method="get">
     <input type="text" name="s" id="search" value="<?php the_search_query(); ?>" />
-    <input type="image" alt="Search" src="<?php bloginfo('template_url'); ?>/images/search.png" />
+    <input type="image" alt="Search" src="/themes/Gathenhielmska/assets/images/Search.svg" />
 </form>
