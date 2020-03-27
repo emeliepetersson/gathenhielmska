7deddd1a5609067f1da0653e96155e0741a0c7eb
emeliepetersson <emepet1204@skola.goteborg.se>
2020-03-27 14:41:39 +0100

    update nav to show all three page-children
    and change show class to show-child for more specificity (if we use show class in more cases)

--- resources/scripts/nav.js
+++ resources/scripts/nav.js
@@ -3,7 +3,7 @@
 const hamburger = document.querySelector(".hamburger");
 const menuItems = document.querySelector(".menu-items");
 const hideChild = document.querySelectorAll(".hide");
-const show = document.querySelectorAll(".show");
+const show = document.querySelectorAll(".show-child");
 
 // Toggle between class names to show/hide menu items and change styling to menu and icon.
 const menu = (function() {
@@ -14,10 +14,11 @@ const menu = (function() {
 
   show[0].addEventListener("click", () => {
     hideChild[0].classList.toggle("showChild");
+    hideChild[1].classList.toggle("showChild");
   });
 
   show[1].addEventListener("click", () => {
-    hideChild[1].classList.toggle("showChild");
+    hideChild[2].classList.toggle("showChild");
   });
 })();
 
