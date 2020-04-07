d80c84c8fc563b3c814f299efeaf426444af569e
Mark <markeddd19@gmail.com>
2020-04-07 17:43:07 +0200

    updated about

--- resources/scripts/about.js
+++ resources/scripts/about.js
@@ -3,4 +3,23 @@
 const showAbout = document.querySelectorAll(".about-hide");
 const icon = document.querySelectorAll(".plus-icon");
 
-export default about;
+console.log(icon);
+console.log(showAbout);
+
+icon[2].addEventListener("click", () => {
+  showAbout[0].classList.toggle("show");
+});
+
+icon[3].addEventListener("click", () => {
+  showAbout[1].classList.toggle("show");
+});
+
+icon[4].addEventListener("click", () => {
+  showAbout[2].classList.toggle("show");
+});
+
+icon[5].addEventListener("click", () => {
+  showAbout[3].classList.toggle("show");
+});
+
+// export default about;
