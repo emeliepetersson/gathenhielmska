18fa07e1574bc81aa532faad2b29105715e5fe5e
emeliepetersson <emepet1204@skola.goteborg.se>
2020-04-08 19:37:04 +0200

    add single event page with styling

---
+++ public/themes/Gathenhielmska/single-mec-events.php
@@ -0,0 +1,51 @@
+<?php get_header(); ?>
+
+<?php if (have_posts()) : ?>
+    <?php while (have_posts()) : the_post(); ?>
+
+
+        <?php
+        $single = new MEC_skin_single();
+        $single_event_main = $single->get_event_mec(get_the_ID());
+        $single_event_obj = $single_event_main[0];
+        ?>
+
+        <div class="single-event-wrapper">
+
+            <h1><?php the_title() ?></h1>
+
+            <div class="buy-wrapper">
+                <?php
+                $image_id = get_post_thumbnail_id();
+                $image_alt = get_post_meta($image_id, '_wp_attachment_image_alt', TRUE);
+                ?>
+                <div class="image-wrapper">
+                    <img src="<?php echo get_the_post_thumbnail_url() ?>" alt=" <?php echo $image_alt ?>">
+                </div>
+
+                <div class="date-wrapper">
+                    <?php $single->display_date_widget($single_event_obj); ?>
+                    <div class="start-time">
+                        <?php
+                        $hours = get_post_meta(get_the_ID(), 'mec_start_time_hour', true);
+                        $minutes = get_post_meta(get_the_ID(), 'mec_start_time_minutes', true);
+                        ?>
+                        <?php die(var_dump(strlen($hours))); ?>
+                    </div>
+                </div>
+                <?php $single->display_location_widget($single_event_obj); ?>
+                <?php echo get_post_meta(get_the_ID(), 'mec_cost', true); ?>
+                <?php $single->display_more_info_widget($single_event_obj); ?>
+            </div>
+            <div class="info-wrapper">
+                <?php $single->display_category_widget($single_event_obj); ?>
+                <?php the_content() ?>
+            </div>
+
+        </div>
+
+    <?php endwhile; ?>
+<?php endif; ?>
+
+
+<?php get_footer(); ?>
