297b4ef24f32b93643304c87908d7f15a0f0fdd4
Mark <markeddd19@gmail.com>
2020-04-09 13:41:24 +0200

    added search page

--- public/themes/Gathenhielmska/search.php
+++ public/themes/Gathenhielmska/search.php
@@ -1,36 +1,18 @@
 <?php get_header(); ?>
-<?php if (have_posts()) : ?>
-
-    <div>
-
-        <?php while (have_posts()) : the_post(); ?>
-
-            <div>
-                <h1><?php the_title(); ?></h1>
-                <?php if (has_post_thumbnail()) : ?>
-                    <div>
-                        <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-                            <?php the_post_thumbnail(); ?>
-                        </a>
+<div class="search-page">
+    <h1>Sökresultat</h1>
+    <?php if (have_posts()) : ?>
+        <div class="search-result">
+            <?php while (have_posts()) : the_post(); ?>
+                <h2><?php the_title(); ?></h1>
+                    <?php $content = get_the_content(); ?>
+                    <p><?php echo wp_trim_words($content, '20'); ?></p>
+                    <div class="search-link">
+                        <button>Event</button>
+                        <a href="<?php the_permalink(); ?>">Länk</a>
                     </div>
-
-                <?php endif; ?>
-                <?php the_content(); ?>
-
-                <div>
-                    <div>
-                        <?php echo previous_post_link(); ?>
-                    </div>
-                    <div>
-                        <?php echo next_post_link(); ?>
-                    </div>
-                </div>
-
-            </div>
-
-        <?php endwhile; ?>
-
-    </div>
-<?php endif; ?>
-
+                <?php endwhile; ?>
+        </div>
+    <?php endif; ?>
+</div>
 <?php get_footer(); ?>
