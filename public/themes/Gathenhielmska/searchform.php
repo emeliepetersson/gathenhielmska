81462c71759741c7acf6f0449ada0e280030aa21
Mark <markeddd19@gmail.com>
2020-04-14 16:32:01 +0200

    updated serach icon

--- public/themes/Gathenhielmska/searchform.php
+++ public/themes/Gathenhielmska/searchform.php
@@ -2,4 +2,4 @@
     <input type="text" id="search-input" name="s" value="<?php the_search_query(); ?>" />
     <input type="image" id="search" alt="Search" src="/themes/Gathenhielmska/assets/images/Search.svg" />
 </form>
-<img class="search-icon" src="/themes/Gathenhielmska/assets/images/Search.svg" alt="search-icon">
+<img class="search-icon" src="<?php echo get_template_directory_uri() . '/assets/images/Search.svg' ?>" alt="search-icon">
